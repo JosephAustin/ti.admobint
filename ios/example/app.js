e207c33dfcfe475a962447ddb34c5478b6e4fd27
Joseph Austin <joseph.the.austin@gmail.com>
2017-05-23 20:29:15 -0500

    Examples

---
+++ ios/example/app.js
@@ -0,0 +1,15 @@
+// All options passed to loadInterstitial are optional.
+var ads = require("ti.admobint");
+
+ads.loadInterstitial({
+  // So that your simulator is a tester
+	testDevices: [ads.SIMULATOR_ID],
+
+  // For targeting ads
+	keywords: ["technology", "mobile"],
+
+  // If you don't supply this it will default to a test-only unit
+	adUnitId: OS_IOS ? '<< ios co-app-pub-xxxx id >>' : '<< android co-app-pub-xxxx id >>'
+});
+
+// Yes that's all you do.
